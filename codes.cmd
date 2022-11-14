e212202bb55a738aee8fdb64e1f2f94f26c67756
krikonis11 <108425658+krikonis11@users.noreply.github.com>
2022-11-14 20:43:35 +0200

    Create codes.cmd

---
+++ codes.cmd
@@ -0,0 +1,15 @@
+cls για διαγραφή 
+ipconfig
+ipconfig /all
+ipconfig /all | find ans DNS 
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+nslookup  networkchuck.com
+nslookup networkchuck.coffee 8.8.8.8
+nslookup  -type-mx networkchuck.com
+getmac /v
+powercfg /energy
+powercfg /batteryreport
+ assoc
+shutdown /r /fw /f /t 0
